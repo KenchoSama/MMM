238f3f243edbe9bcf263748e125a71ad83adea1f
sirbiscuit87 <135094905+sirbiscuit87@users.noreply.github.com>
2024-04-02 15:04:22 -0700

    Added players to each gamemode

--- Reusable Scenes/Player/Scripts/CustomBody.gd
+++ Reusable Scenes/Player/Scripts/CustomBody.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
+# Made by victor, script for character customization, don't change.
+# 
 # Texture variables given in drawing order
-var skin_texture: Texture2D
-var hat_texture: Texture2D
-var facialhair_texture: Texture2D
-var shirt_texture: Texture2D
-var leg_texture: Texture2D
+var face_texture: Texture2D = preload("res://Reusable Scenes/Player/Sprites/Face/DefaultFace.png")
+var hat_texture: Texture2D = preload("res://Reusable Scenes/Player/Sprites/Hats/DefaultHat.png")
+var facialhair_texture: Texture2D = preload("res://Reusable Scenes/Player/Sprites/Facial Hair/DefaultBeard.png")
+var hand_texture: Texture2D = preload("res://Reusable Scenes/Player/Sprites/Hand/DefaultHand.png")
+var shirt_texture: Texture2D = preload("res://Reusable Scenes/Player/Sprites/Shirts/DefaultRobe.png")
 
+# Character customization options will be an array of filepath strings
+
+# TODO load character customization options from user disk
+func load_customization_options():
+	pass
+
+# Just a call to update the sprites. Should be called whenever customization options are changed.
+func update_sprites():
+	$Face.texture = face_texture
+	$Hat.texture = hat_texture
+	$"Facial Hair".texture = facialhair_texture
+	$Hand.texture = hand_texture
+	$Shirt.texture = shirt_texture
