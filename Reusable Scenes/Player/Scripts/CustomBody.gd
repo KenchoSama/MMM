6e357951b36c43acf3d82daf333386e2bf34c7a3
sirbiscuit87 <135094905+sirbiscuit87@users.noreply.github.com>
2024-04-02 14:32:45 -0700

    Changes to filesystem, changed antialiasing

---
+++ Reusable Scenes/Player/Scripts/CustomBody.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+# Texture variables given in drawing order
+var skin_texture: Texture2D
+var hat_texture: Texture2D
+var facialhair_texture: Texture2D
+var shirt_texture: Texture2D
+var leg_texture: Texture2D
+
---
+++ Reusable Scenes/Player/Sprites/Vehicles/Readme.txt
@@ -0,0 +1,2 @@
+Player could have different vehicle customization options if the game just requires to float around.
+He could use a broomstick, a surfboard, a hoverboard, whatever we want to draw
