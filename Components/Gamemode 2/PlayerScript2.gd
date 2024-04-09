e0089bb4bb87cdb824fe768a00fb7d6ace527dc9
Connor Penn <cnnrpenn21@gmail.com>
2024-04-09 00:53:12 -0700

    updated PlayerScript2
    changed from getters to vars containing sprites

--- Components/Gamemode 2/PlayerScript2.gd
+++ Components/Gamemode 2/PlayerScript2.gd
@@ -6,6 +6,21 @@ var acceleration = 0.01
 var maxSpeed = 1200
 var target = position
 
+# used for preventing unnecessary math
+var face
+var hat
+var shirt
+var hand
+var facialHair
+var legs
+
+func _ready():
+	face = $Sprites/Face
+	hat = $Sprites/Hat
+	shirt = $Sprites/Shirt
+	hand = $Sprites/Hand
+	facialHair = $"Sprites/Facial Hair"
+	legs = $Sprites/Legs
 func _input(event):
 	if event is InputEventScreenTouch and event.is_pressed():
 		target = event.get_position()
@@ -20,19 +35,19 @@ func _physics_process(delta):
 	
 	velocity = position.direction_to(target) * speed
 	if velocity.x < 0:
-		$Sprites/Face.flip_h = true
-		$Sprites/Hat.flip_h = true
-		$Sprites/Shirt.flip_h = true
-		$Sprites/Hand.flip_h = true
-		$"Sprites/Facial Hair".flip_h = true
-		$Sprites/Legs.flip_h = true
+		face.flip_h = true
+		hat.flip_h = true
+		shirt.flip_h = true
+		hand.flip_h = true
+		facialHair.flip_h = true
+		legs.flip_h = true
 	elif velocity.x > 0:
-		$Sprites/Face.flip_h = false
-		$Sprites/Hat.flip_h = false
-		$Sprites/Shirt.flip_h = false
-		$Sprites/Hand.flip_h = false
-		$"Sprites/Facial Hair".flip_h = false
-		$Sprites/Legs.flip_h = false
+		face.flip_h = false
+		hat.flip_h = false
+		shirt.flip_h = false
+		hand.flip_h = false
+		facialHair.flip_h = false
+		legs.flip_h = false
 	if position.distance_to(target) > 10:
 		move_and_slide()
 
