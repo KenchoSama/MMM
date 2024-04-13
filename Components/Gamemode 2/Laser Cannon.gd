c14f5d0bedcb7bd5a57540ef3b0a5cd8a1ce23f7
Connor Penn <cnnrpenn21@gmail.com>
2024-04-13 16:13:21 -0700

    laser ray assets added, creating particles

--- Components/Gamemode 2/Laser Cannon.gd
+++ Components/Gamemode 2/Laser Cannon.gd
@@ -1,13 +1,41 @@
 extends Node2D
 
+var timer
+var timerStopped
+var timePassed
+var countDown
+var reload
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	timer = $Timer
+	timePassed = 0
+	timerStopped = true
+	reload = true
+	_cannon_timer()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	timePassed += delta
+	if reload:
+		#TODO load first equation and start tim
+		reload = false
+		pass
+	if timerStopped:
+		
+		timer.start(countDown)
+		timerStopped = false
 
 func _enemy_position():
+	pass
 	
+func _cannon_timer():
+	if timePassed < 20:
+		countDown = 5
+	elif timePassed < 40:
+		countDown = 2.5
+	else:
+		countDown = 1
+
+func _on_timer_timeout():
+	timerStopped = true
--- Components/Gamemode 2/RayCast2D.gd
+++ Components/Gamemode 2/RayCast2D.gd
@@ -1,21 +1,41 @@
 extends RayCast2D
 
 var target
-var timer
+var laserTimer
+var timePassed
+var tween
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer = $Timer # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	var cast_point = target
-	force_raycast_update
-	
-	if is_colliding():
-		cast_point = to_local(get_colission_point())
-		
-	#TODO update target location correctly
+	set_physics_process(false)
+	laserTimer = $LaserTimer # Replace with function body.
+	timePassed = 0
+	tween = create_tween()
 
 func _function_shoot():
-	#TODO casting ray based on time intervals
+	laserTimer.start(2)
+	_appear()
+	set_physics_process(true)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	target = Vector2(500,-500)
+	var cast_point = target
+	force_raycast_update()
+	
+	if is_colliding():
+		cast_point = get_collision_point()
+		
+	#TODO update target location correctly
+	if laserTimer.is_stopped():
+		_disappear()
+		set_physics_process(false)
+
+func _appear() -> void:
+	tween = create_tween()
+	tween.stop_all()
+	tween.interpolate_property($Line2D, "width", 10.0, 0, 0.1)
+	tween.start()
+func _disappear() -> void:
+	tween.stop_all()
+	tween.interpolate_property($Line2D, "width", 10.0, 0, 0.1)
+	tween.start()
