2f841ab38a0ab7a37121474c43b2d9711d06b86b
Connor Penn <cnnrpenn21@gmail.com>
2024-04-16 01:07:16 -0700

    gamemode 2 working with bugs

--- Components/Gamemode 2/Laser Cannon.gd
+++ Components/Gamemode 2/Laser Cannon.gd
@@ -1,40 +1,32 @@
 extends Node2D
 
 var timer
-var timerStopped
 var timePassed
+var reloadTimer
 var countDown
 var reload
 var rng
 var xMulti
 var xOffset
 var laserTarget
+var string
+var stringActual
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	timer = $Timer
+	reloadTimer = $Timer2
 	timePassed = 0
-	timerStopped = true
-	reload = true
-	rng = RandomNumberGenerator.new()
 	_cannon_timer()
+	timer.start(1)
+	rng = RandomNumberGenerator.new()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	timePassed += delta
-	if reload:
-		xMulti = rng.randi_range(-3,3)
-		xOffset = rng.randi_range(-3,3) * 54
-		position.x = 252
-		position.y = 324 + 324 * xMulti - xOffset
-		laserTarget = Vector2(1152, (xMulti*(-1152+xOffset)))
-		reload = false
-	if timerStopped:
-		$RayCast2D._function_shoot()
-		_cannon_timer()
-		timer.start(countDown)
-		timerStopped = false
-		reload = true
+	$"../Label".set_text(str(int(timePassed)))
+	
 	
 func _cannon_timer():
 	if timePassed < 20:
@@ -44,5 +36,29 @@ func _cannon_timer():
 	else:
 		countDown = 1
 
+
+
 func _on_timer_timeout():
-	timerStopped = true
+	$RayCast2D._fucntion_reload()
+	xMulti = rng.randi_range(-3,3)
+	xOffset = rng.randi_range(-5,5) * 54
+	laserTarget = Vector2(648, (xMulti*(-648-xOffset)))
+	if -1*xOffset < 0:
+		string = "Y = %dX - %d"
+		stringActual = string % [xMulti, xOffset/54]
+	elif -1*xOffset > 0:
+		string = "Y = %dX + %d"
+		stringActual = string % [xMulti, -1*xOffset/54]
+	else:
+		string = "Y = %dX"
+		stringActual = string % xMulti
+	$"../Equation".set_text(stringActual)
+	reloadTimer.start(2)
+
+
+func _on_timer_2_timeout():
+	_cannon_timer()
+	position.x = 252
+	position.y = 324 + (324 - xOffset) * xMulti
+	$RayCast2D._function_shoot()
+	timer.start(countDown)
--- Components/Gamemode 2/RayCast2D.gd
+++ Components/Gamemode 2/RayCast2D.gd
@@ -1,39 +1,38 @@
 extends RayCast2D
 
 var target
-var laserTimer
 var timePassed
 var tween
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(false)
-	laserTimer = $LaserTimer # Replace with function body.
+	add_exception($"../../StaticBody2D")
 	timePassed = 0
 	tween = create_tween()
 
 func _function_shoot():
-	laserTimer.start(2)
 	_appear()
 	set_physics_process(true)
+	
+func _fucntion_reload():
+	_disappear()
+	set_physics_process(false)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	target = $"..".laserTarget
 	target_position = target
-	$Line2D.points[1] = target - position
+	$Line2D.points[1] = target_position
 	force_raycast_update()
 	
 	if is_colliding():
 		target_position = get_collision_point()
-	if laserTimer.is_stopped():
-		_disappear()
-		set_physics_process(false)
 
 func _appear() -> void:
-	tween.stop()
+	tween.kill()
 	tween.tween_property($Line2D, "width", 10.0, 0.1)
 	tween.play()
 func _disappear() -> void:
-	tween.stop()
+	tween.kill()
 	tween.tween_property($Line2D, "width", 0, 0.1)
 	tween.play()
