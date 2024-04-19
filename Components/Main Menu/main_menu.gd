c7680360f0fdbc9aabcb1de7c0b19faf462eef00
sirbiscuit87 <135094905+sirbiscuit87@users.noreply.github.com>
2024-04-18 22:54:49 -0700

    Added mute button to savefile

--- Components/Main Menu/main_menu.gd
+++ Components/Main Menu/main_menu.gd
@@ -5,11 +5,12 @@ extends Control
 # High scores
 # Currency (when implemented)
 var streamPlayer
+var PlayerData: ConfigFile # Playerdata is global so the save fail remains open 
 # If there is no PlayerData file, then it's the user's first time booting the game. Initialize it.
 func _ready():
 	Engine.max_fps = 60
 	RenderingServer.set_default_clear_color(Color.CYAN)
-	var PlayerData = ConfigFile.new()
+	PlayerData = ConfigFile.new() 
 	var err = PlayerData.load("user://PlayerData.cfg")
 	streamPlayer = $AudioStreamPlayer2D
 	
@@ -19,9 +20,14 @@ func _ready():
 		PlayerData.set_value("CustomSprites", "hat", "res://Reusable Scenes/Player/Sprites/Hats/Hat2.png")
 		PlayerData.set_value("CustomSprites", "facialhair", "res://Reusable Scenes/Player/Sprites/Facial Hair/Beard1.png")
 		PlayerData.set_value("CustomSprites", "shirt", "res://Reusable Scenes/Player/Sprites/Shirts/Shirt1.png")
+		PlayerData.set_value("Settings", "mute_music", false)
 		PlayerData.save("user://PlayerData.cfg")
 		# TODO set appropriate high scores, currency to default
 	
+	if PlayerData.get_value("Settings", "mute_music"): #Otherwise, it'll just be normal
+		$MuteButton.icon = load("res://assets/muted.png") 
+		streamPlayer.volume_db = -99.0
+		
 	# TODO read appropriate high scores, currency
 	
 func _process(_delta):
@@ -50,10 +56,15 @@ func _on_button_for_cc_button_up():
 	get_tree().change_scene_to_file("res://Components/Character Customizer/character_customization.tscn")
 
 func _on_mute_button_button_up():
-	if streamPlayer.volume_db == -99.0:
+	if streamPlayer.volume_db == -99.0: # Muted to unmuted
+		$MuteButton.icon = load("res://assets/unmuted.png")
 		streamPlayer.volume_db = -32.0
-	elif streamPlayer.volume_db == -32.0:
+		PlayerData.set_value("Settings", "mute_music", false)
+	elif streamPlayer.volume_db == -32.0: # Unmuted to muted
+		$MuteButton.icon = load("res://assets/muted.png")
 		streamPlayer.volume_db = -99.0
+		PlayerData.set_value("Settings", "mute_music", true)
+	PlayerData.save("user://PlayerData.cfg")
 
 
 func _on_audio_stream_player_2d_finished():
