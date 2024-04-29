a432cdf7eafb97dc2a66f769b7c72e2af3997993
kenchosama <mr.kenlopezmartinez@gmail.com>
2024-04-28 17:47:17 -0700

    explosion on collision for fire

--- Components/Gamemode 3/PlayerScript3.gd
+++ Components/Gamemode 3/PlayerScript3.gd
@@ -1,89 +1,129 @@
 extends CharacterBody2D
-# Write code for the player in gamemode 3 here
-var wizard: bool = true
-var spellcooldown: bool = true
-var primary_action_pressed = false  # Flag to track primary action press state
+
+const minSpeed = 300
+const maxSpeed = 800
 var speed = 300
-#fire spells
+var acceleration = 0.03
+var target = position
+
+# used for preventing unnecessary math
+var spellcooldown: bool = true
 signal lost
 signal firespellActivated(pos, direction)
 signal spellReleased
 signal lightingspellActivated(pos, direction)
 signal waterspellActivated(pos, direction)
 
-func _process(delta):
+func _ready():
+	set_physics_process(false)
+
+func _input(event):
+	if event is InputEventScreenTouch and event.is_pressed():
+		target = event.get_position()
+	elif event is InputEventScreenDrag:
+		target = event.get_position()
+
+func _physics_process(_delta):
+	if position.distance_to(target) > 150 and speed < maxSpeed:
+		speed = speed + speed * acceleration
+	elif position.distance_to(target) < 50 and speed > minSpeed:
+		speed = speed - speed * acceleration
 	
-	#input for move
-	#var direction = Input.get_vector("left", "right", "up", "down")
-	#position += direction * delta * 400
-	#move_and_slide()
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
-	var motion = input_vector * speed * delta
-	var collision = move_and_collide(motion)
+	velocity = position.direction_to(target) * speed
 	
-	#rotate wizard
-	look_at(get_global_mouse_position())
-	
-	#activation of spells
-	var spell_type = get_mouse_input_action()
-	var spell_marker = $spellposition.get_children()
-	var player_direction = (get_global_mouse_position() - position).normalized()
 	
 	var player_sprite = $Sprites
 	# Check if the player direction's x component is negative
+	var player_direction = (get_global_mouse_position() - position).normalized()
 	if player_direction.x < 0:
 		player_sprite.scale.y = -1
 	else:
 	# Reset the player sprite scale if the direction is not negative
 		player_sprite.scale.y = 1
+	
+	
+	if position.distance_to(target) > 20:
+		move_and_slide()
+		
+		#rotate wizard
+	look_at(get_global_mouse_position())
+	#activation of spells
+	#var spell_type = get_mouse_input_action()
+	var spell_marker = $spellposition.get_children()
 
-
-
-	if spell_type == "primary" and spellcooldown:
+	if Input.is_key_pressed(KEY_Q) and spellcooldown:
 		spellcooldown = false
 		$SpellTimer1.start()
 		firespellActivated.emit(spell_marker[0].global_position, player_direction)
-		
-		
-	if spell_type == "secondary" and spellcooldown:
-		#print("lighting spell")
+	elif Input.is_key_pressed(KEY_W) and spellcooldown:
 		spellcooldown = false
 		$SpellTimer1.start()
 		lightingspellActivated.emit(spell_marker[0].global_position, player_direction)
-		
-		
-	if spell_type == "thirdAction" and spellcooldown:
-		#print("lighting spell")
+	elif Input.is_key_pressed(KEY_E) and spellcooldown:
 		spellcooldown = false
 		$SpellTimer1.start()
 		waterspellActivated.emit(spell_marker[0].global_position, player_direction)
-
-
-
+		
 
 func _on_spell_timer_1_timeout():
 	spellcooldown = true
-		
 
 
 
+#func _input(event):
+	#if event is InputEventScreenTouch and event.is_pressed():
+		#target = event.get_position()
+	#elif event is InputEventScreenDrag:
+		#target = event.get_position()
+#
+#
+#func _physics_process(_delta):
+	#if position.distance_to(target) > 150:
+		#speed = speed + speed
+	#elif position.distance_to(target) < 50:
+		#speed = speed - speed
+	#
+	#velocity = position.direction_to(target) * speed
+	#if position.distance_to(target) > 20:
+		#move_and_slide()
+		#
+	#var direction = (target - position).normalized() * speed
+	#var player_sprite = $Sprites
+	#if direction.x < 0:
+		#player_sprite.scale.y = -1
+	#else:
+		#player_sprite.scale.y = 1
+		#
+		#
+		#
+#func _process(delta):
+#
+#
+	#var input_vector = Vector2.ZERO
+	#input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	#input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	#input_vector = input_vector.normalized()
+	#var motion = input_vector * speed * delta
+	#var collision = move_and_collide(motion)
 
-func get_mouse_input_action():
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		return "primary"
-	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-		return "secondary"
-	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
-		return "thirdAction"
-	else:
-		return "no_input"
 
-
+##func get_mouse_input_action():
+	##if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		##return "primary"
+	##elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		##return "secondary"
+	##elif Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
+		##return "thirdAction"
+	##else:
+		##return "no_input"
+#
+#
 func _on_area_2d_body_entered(body):
 	if "triangle" in body:
 		queue_free()
 		print("body was crushed")
 		emit_signal("lost")
+
+
+func _on_start_button_up():
+	set_physics_process(true)
--- Components/Gamemode 3/fire.gd
+++ Components/Gamemode 3/fire.gd
@@ -7,22 +7,29 @@ var direction = Vector2.RIGHT
 func _ready():
 	# Start a Timer to delete the node after 2 seconds
 	$Timer.start()
+	$explosion.visible = false
 
 func _process(delta):
 	position += direction * delta * speed
 	
 
-
-
 func _on_area_2d_area_entered(area):
 	queue_free()
+	print("explode")
 	
 
 
 func _on_area_2d_body_entered(body):
 	body.hit()
-	queue_free()
+	$Sprite2D.visible = false
+	explode()
+	
 
 
 func _on_timer_timeout():
 	queue_free()
+
+func explode():
+	speed = 0
+	$explosion.visible = true
+	$AnimationPlayer.play("explosion")
