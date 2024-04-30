927fd75e541cc942c4b1b4f2f42c04eea4e7bad2
kenchosama <mr.kenlopezmartinez@gmail.com>
2024-04-30 13:27:11 -0700

    animations for other spells

--- Components/Gamemode 3/lighting.gd
+++ Components/Gamemode 3/lighting.gd
@@ -1,5 +1,4 @@
-extends Area2D
-
+extends Node2D
 
 @export var speed: int = 300
 var direction = Vector2.RIGHT
@@ -8,22 +7,30 @@ var direction = Vector2.RIGHT
 func _ready():
 	# Start a Timer to delete the node after 2 seconds
 	$Timer.start()
+	$lightingExplosion.visible = false
 
 func _process(delta):
 	position += direction * delta * speed
 	
 
-
-
 func _on_area_2d_area_entered(area):
 	queue_free()
+	print("explode")
 	
 
 
 func _on_area_2d_body_entered(body):
-	body.hit()
-	queue_free()
+	if "triangle" in body:
+		body.hit()
+		$Sprite2D.visible = false
+		explode()
+	
 
 
 func _on_timer_timeout():
 	queue_free()
+
+func explode():
+	speed = 0
+	$lightingExplosion.visible = true
+	$AnimationPlayer.play("lightinexplosion")
