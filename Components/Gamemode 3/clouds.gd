0d762b72fc7aad136c5bc325a57f9da9299bd246
kenchosama <mr.kenlopezmartinez@gmail.com>
2024-05-05 13:33:41 -0700

    4.1.1 game3 hints and clouds

--- Components/Gamemode 3/clouds.gd
+++ Components/Gamemode 3/clouds.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+var speed_min = 50
+var speed_max = 150
+var speed = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Set a random speed between speed_min and speed_max
+	speed = randf_range(speed_min, speed_max)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# Move the clouds to the left
+	var movement = Vector2.RIGHT * speed * delta
+	translate(movement)
